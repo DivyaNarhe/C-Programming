da817c7fd634570ae0b510a57caf1e1e4f726541
DivyaNarhe <89540581+DivyaNarhe@users.noreply.github.com>
2021-08-29 22:44:18 -1200

    FunctionPointer

--- function.c
+++ function.c
@@ -9,20 +9,39 @@ bool ChkPrime(int N)
         return 0;
     }
 }
+int Maximum(int iNo1,int iNo2){
+    if(iNo1>iNo2){
+        return iNo1;
+    }
+    else{
+        return iNo2;
+    }
+}
 
 int main(){
-    auto int a;
+    auto int a=0;
     bool bret=0;
     printf("Enter number:\n");
     scanf("%d",&a);
-    bret =ChkPrime(a);
+    bool (*Primeptr)(int);
+    Primeptr=ChkPrime;
+    bret =Primeptr(a);
     if(bret==1){
         printf("Even Number.");
     }
     else{
         printf("Odd Number.");
     }
-
-
+   auto int i=0,j=0,Mret=0;
+    printf("\n****** Maximum function starts ********");
+    printf("\nEnter 1st number:");
+    scanf("%d",&i);
+    printf("Enter 2nd number:");
+    scanf("%d",&j);
+    int (*Maxptr)(int ,int);
+    Maxptr=Maximum;
+    Mret=Maxptr(i,j);
+    printf("Maximum Number is :%d",Mret);
+    return 0;
 
 }
