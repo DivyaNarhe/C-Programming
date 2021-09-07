907cc214c2944b0b305acf4a831d27a2da1ae3a7
DivyaNarhe <89540581+DivyaNarhe@users.noreply.github.com>
2021-09-06 23:20:29 -1200

    For loop

---
+++ que2.c
@@ -0,0 +1,16 @@
+// Print 'jay Ganesh' 5 times on screen
+#include<stdio.h> 
+void Display() 
+{ 
+ int i = 0; 
+ for(i = 1; i<=5 ;i++) 
+ { 
+ printf("Jay Ganesh\n"); 
+ } 
+}
+int main() 
+{ 
+ 
+ Display(); 
+ return 0; 
+}
---
+++ que3.c
@@ -0,0 +1,16 @@
+//Print 5 to 1 number on screen using for loop 
+#include<stdio.h> 
+int Display() 
+{ 
+ int i =5; 
+ for( i=5;i>=1 ;i--) 
+ { 
+ printf("%d\n",i);  
+ } 
+} 
+int main() 
+{ 
+ Display(); 
+ 
+ return 0; 
+}
